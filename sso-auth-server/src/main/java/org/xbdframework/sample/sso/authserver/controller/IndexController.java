f01d829017caa64f76e9138bd8578dc58bb68fb3
liuminglei <812934656@qq.com>
2020-06-09 09:34:39 +0800

    授权系统添加首页，展示OA、CRM的链接

---
+++ sso-auth-server/src/main/java/org/xbdframework/sample/sso/authserver/controller/IndexController.java
@@ -0,0 +1,34 @@
+package org.xbdframework.sample.sso.authserver.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class IndexController {
+
+    @Value("${spring.application.name}")
+    private String name;
+
+    @Value("${spring.application.version}")
+    private String version;
+
+    @Value("${oa.profile-uri}")
+    public String oaProfileUri;
+
+    @Value("${crm.profile-uri}")
+    public String crmProfileUri;
+
+    @RequestMapping("/index")
+    public String index(Model model) {
+        model.addAttribute("name", name);
+        model.addAttribute("version", version);
+        model.addAttribute("oaProfileUri", oaProfileUri);
+        model.addAttribute("crmProfileUri", crmProfileUri);
+
+        return "index";
+    }
+
+}
